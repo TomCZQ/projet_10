08456c1cedca6a53124db48016a327966835dea4
TomCZQ <thomasdevpro38@gmail.com>
2024-07-08 15:48:26 +0200

    feat : initiated routes and first components (header footer)

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
 import './App.css';
+import Header from "../src/"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <MainApp />
+      </Router>
+  );
+};
+
+function MainApp() {
+  return (
+      <main className="full-height">
+        <Header/>
+        <div className="">
+          <Route path="/login" element="Login" />
+          <Route path="/home" element="Home" />
+          <Route path="/profile" element="Profile" />
+        </div>
+        <Footer/>
+      </main>
   );
 }
 
